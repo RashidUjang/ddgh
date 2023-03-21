f44c0d20ec1a4ebdab62e66d51296cbe19dff5de
Adam Rashid <rashid.uj@gmail.com>
2023-03-21 22:34:05 +0800

    chore: moved the project folder structure to be in /src

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+
+import roomRouter from './routes/v1/roomRouter.js'
+
+dotenv.config()
+const app = express()
+
+app.use(roomRouter)
+
+app.listen(process.env.PORTs, () => {
+    console.log(`Listening on port ${process.env.PORT}.`)
+})
